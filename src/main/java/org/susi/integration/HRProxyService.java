cda350a39f010ab8cf4bf7b93ee14dc89b409b86
susinda <susinda@gmail.com>
2021-08-28 14:03:33 +0530

    Pizza service chend to employee data service

--- src/main/java/org/susi/integration/HRProxyService.java
+++ src/main/java/org/susi/integration/HRProxyService.java
@@ -8,11 +8,11 @@ import org.springframework.integration.http.config.EnableIntegrationGraphControl
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableIntegrationGraphController(allowedOrigins = "*")
-public class PizzaServiceDemo {
+public class HRProxyService {
 
 	
 	public static void main(String[] args) {
-		SpringApplication.run(PizzaServiceDemo.class, args);
+		SpringApplication.run(HRProxyService.class, args);
 	}
 }
 
--- src/test/java/org/susi/integration/EurekaClientApplicationTests.java
+++ src/test/java/org/susi/integration/EurekaClientApplicationTests.java
@@ -6,12 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.susi.integration.PizzaServiceDemo;
+import org.susi.integration.HRProxyService;
 import junit.framework.Assert;
 
  
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = PizzaServiceDemo.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = HRProxyService.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class EurekaClientApplicationTests {
 

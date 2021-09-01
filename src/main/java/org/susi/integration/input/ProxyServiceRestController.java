8eb486e6980377997a36835effbf4000ffc6ff2c
susinda <susinda@gmail.com>
2021-09-01 09:21:18 +0530

    get endpoint added

--- src/main/java/org/susi/integration/input/ProxyServiceRestController.java
+++ src/main/java/org/susi/integration/input/ProxyServiceRestController.java
@@ -2,6 +2,7 @@ package org.susi.integration.input;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.jms.TextMessage;
 import org.apache.http.client.methods.HttpPost;
@@ -13,6 +14,8 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +35,11 @@ public class ProxyServiceRestController  extends CodeobeListener  {
 	
 	@Value("${output.http_endpoint}")
 	String outputEndpoint;
+	
+	@GetMapping("/hello/{user}")
+	public String hello(@PathVariable String user) {
+		return "Hello " +  user + new Date().toString();
+	}
 
 	@PostMapping(value = "/order")
 	public String order(@RequestBody String request) {
